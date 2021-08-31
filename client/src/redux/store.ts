4893e14cd806edbb1087ef660503ccaec791eb68
Evgeny <evtsyga@gmail.com>
2021-08-31 18:48:52 +0300

    remove redux logger from prod

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, Middleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import reducer from "./reducer";
 
-const enhancer = applyMiddleware(thunk, logger);
+function setReduxMiddleware(logger: Middleware, ...middleware: Middleware[]): Middleware[] {
+    if (process.env.NODE_ENV !== 'production') {
+        return [...middleware, logger];
+    } else {
+        return [...middleware];
+    }
+}
+
+const enhancer = applyMiddleware(...setReduxMiddleware(logger, thunk));
 const store = createStore(reducer, enhancer);
 
 export default store;
